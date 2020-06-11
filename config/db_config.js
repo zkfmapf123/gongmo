78a4ad9b7052edf410a455b8ebe92323357b7d20
zkfmapf999 <zkfmapf123@naver.com>
2020-06-11 17:57:11 +0900

    commit

---
+++ config/db_config.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+module.exports = {
+    development:{
+      username : process.env.MYSQL_USER,
+      password : process.env.MYSQL_PASSWORD,
+      database : process.env.MYSQL_DATABASE,
+      host : process.env.MYSQL_HOST,
+      dialect : "mysql",
+      operatorsAliases : false,
+    },
+
+    production:{
+      username : process.env.MYSQL_USER,
+      password : process.env.MYSQL_PASSWORD,
+      database : process.env.MYSQL_DATABASE,
+      host : process.env.MYSQL_HOST,
+      dialect : "mysql",
+      operatorsAliases : false,
+      logging : false
+    },
+}
